fceef8e82b0f3cd9778457b0f072a8c745df1aa2
Waltero Estoplero <waltser@web.de>
2019-10-16 14:01:22 +0200

    My first commit

---
+++ FHL-Windows-Forms-Leer-001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Nun kommt ein Formular:");
+
+        Form ersteForm = new Form();
+        Application.Run(ersteForm);
+    }
+}
